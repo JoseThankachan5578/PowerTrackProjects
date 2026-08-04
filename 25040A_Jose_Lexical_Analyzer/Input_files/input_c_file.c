fd0ffb69505b1eee01b3b810f06ff812357bc3b8
Jose <joseolickal2001@gmail.com>
2026-08-05 00:22:01 +0530

    Initial commit: Lexical_Analyzer (input files)

---
+++ 25040A_Jose_Lexical_Analyzer/Input_files/input_c_file.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int print_count(void)
+{
+
+    int count = 0;
+    int a =10;
+    int value= 125;
+    int oct_val1 = 0798;
+    int oct_val = 0746;
+    int hex_val1 = 0x5HF;
+    int hex_val = 0X1FD;
+    printf(("Count value is %d\n", count);
+
+    while(1))
+    {
+        printf("Hello\n");
+    }
+
+    return 0;
+    
+}
+}
