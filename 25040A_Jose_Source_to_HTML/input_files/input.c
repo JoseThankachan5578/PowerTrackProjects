5ebd788c46fbb52b6e435d5a793e784505173d14
Jose <joseolickal2001@gmail.com>
2026-09-12 23:12:27 +0530

    Initial commit: Source_to_HTML

---
+++ 25040A_Jose_Source_to_HTML/input_files/input.c
@@ -0,0 +1,39 @@
+//This is the input file for Source to html converter project
+
+/*
+Source to html converter project
+Name: Jose Jose
+Whether 2+2=4 correct?
+Is 2<3 true?
+This is an example of using <stdio.h>
+Hope you get it!
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int number;
+int a = 0xAC;
+
+float b = 10.2;
+double c = 10.254878;
+
+
+int m = 10/3;
+int n = m / 2;
+
+int main()
+{
+    if(x==10)
+    {
+        printf("Hi\n");
+    }
+    else 
+    {
+        printf("Today is a good day\n");
+    }
+
+    printf("This project was awesome\n");
+
+}
